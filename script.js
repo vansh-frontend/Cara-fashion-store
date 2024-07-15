cd0f805fcd16568fd701ad29a63948a1f03100bd
vansh-frontend <vanshdhalor04>
2024-07-15 22:55:56 +0530

    first

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ emailField.focus({
   preventScroll: true,
 });
 
+// navbar
+
+document.getElementById("hamburger").addEventListener("click", function () {
+  var mobileNav = document.getElementById("mobile-nav");
+  if (mobileNav.classList.contains("open")) {
+    mobileNav.classList.remove("open");
+  } else {
+    mobileNav.classList.add("open");
+  }
+});
